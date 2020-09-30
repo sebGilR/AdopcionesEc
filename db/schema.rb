f45eb1c4d7a2801713e17f77dfc81d4af18c5c42
Sebastian Gil Rodriguez <lbuggie.21@gmail.com>
2020-09-30 12:47:36 -0500

    Initialize user and pet models

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_28_192611) do
+ActiveRecord::Schema.define(version: 2020_09_30_153736) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "pets", force: :cascade do |t|
+    t.string "type"
+    t.string "size"
+    t.text "description"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
