fc3e09429d0dc3c727c16cbe4e5cc1f6b9145528
Sebastian Gil Rodriguez <lbuggie.21@gmail.com>
2020-10-12 10:42:51 -0500

    Set up react folder structure

--- app/javascript/packs/components/App.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const App = () => (
-  <div>Hi from AdopcionesEc</div>
-)
-
-export default App;
---
+++ app/javascript/packs/components/App/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import CssBaseline from '@material-ui/core/CssBaseline';
+
+const App = () => (
+  <>
+    <CssBaseline />
+    <div>Hi from AdopcionesEc</div>
+  </>
+)
+
+export default App;
